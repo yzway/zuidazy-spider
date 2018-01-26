fd5a19ee1596dedd97268b3bb8e73e4d9f04cc11
tiandaxyf <tiandaxyf@gmail.com>
2018-01-26 16:49:40 +0800

    增加抓取图片方法

--- source.py
+++ source.py
@@ -8,6 +8,7 @@
 """
 
 import codecs
+import urllib
 
 import requests
 from bs4 import BeautifulSoup
@@ -52,18 +53,33 @@ def detail_html(html):
     warp_dom = soup.find('div', attrs={'class':'warp'})
     title = warp_dom.find('div', attrs={'class':'vodh'}).find('h2').text
     thumb = warp_dom.find('div', attrs={'class':'vodImg'}).find('img').attrs['src']
-    source_url = PARSE_URL + warp_dom.find('div', attrs={'id':'play_1'}).find('input', attrs={'name':'copy_sel'}).attrs['value']
+    source_url = warp_dom.find('div', attrs={'id':'play_1'}).find('input', attrs={'name':'copy_sel'}).attrs['value']
     createTime = warp_dom.find('div', attrs={'class':'vodinfobox'}).find('ul').contents[17].find('span').text
-    _s = ["'" + title + "'", "'" + thumb + "'", "'" + source_url + "'", "'" + createTime + "'"]
-    return _s
+    area = warp_dom.find('div', attrs={'class':'vodinfobox'}).find('ul').contents[9].find('span').text
+    language = warp_dom.find('div', attrs={'class': 'vodinfobox'}).find('ul').contents[11].find('span').text
+    release = warp_dom.find('div', attrs={'class': 'vodinfobox'}).find('ul').contents[13].find('span').text
+    # 过滤图片可成功下载的资源
+    if thumb.find("http://tupian.tupianzy.com/pic/upload/vod/201") != -1 and title.strip() and source_url.strip() and createTime.strip():
+        _s = ["'mytitle" + title + "'", "'" + thumb + "'", "'" + source_url + "'", "'" + area + "'",
+              "'" + language + "'", "'" + release + "'", "'" + createTime + "mytime'"]
+        return _s
 
-
+# 分析详情页的缩略图
+def parse_detail_pic(html):
+    soup = BeautifulSoup(html, 'lxml')
+    warp_dom = soup.find('div', attrs={'class': 'warp'})
+    thumb = warp_dom.find('div', attrs={'class': 'vodImg'}).find('img').attrs['src']
+    # 过滤图片可成功下载的链接
+    if thumb.find("http://tupian.tupianzy.com/pic/upload/vod/201") != -1:
+        _s = [thumb]
+        return _s
 
 def main():
     url = DOWNLOAD_URL
     html = []
     a_href_list = []
     next_page_url = ''
+
     # 获取所有详情页的链接并写入到detail_urls文件中
     # with codecs.open('detail_urls', 'wb', encoding='utf-8') as fp:
     #     #当前资源网的伦理片页数为48
@@ -79,28 +95,43 @@ def main():
 
 
     # 解析所有页面并写入到source文件中
-    fp = open('detail_urls', 'r')
-    contents = fp.read()
+    items = []
+    contents = []
+    with codecs.open('detail_urls', 'r', encoding='utf-8') as fp:
+        contents = fp.read()
+        items = contents.split('\n')
 
-    items = contents.split('\n')
     r_content = ''
+    # with codecs.open('source', 'wb', encoding='utf-8') as fp_source:
+    #     if items:
+    #         for index, item in enumerate(items):
+    #             if item:
+    #                 html2 = download_page(item)
+    #                 if html2:
+    #                     source = detail_html(html2)
+    #                     raw_string = ','.join(source)  # list拆分成字符串并以逗号连接\
+    #                     r_content = str(raw_string) + '\n'
+    #                     fp_source.write(r_content)
+    #                     print(index+1)
 
-    with codecs.open('source', 'wb', encoding='utf-8') as fp_source:
+    # 解析图片下载地址列表写入pics文件中
+    with codecs.open('pics', 'wb', encoding='utf-8') as fp_pic:
         if items:
-            i= 0
             for index, item in enumerate(items):
+                x = 1
                 if item:
-                    html2 = download_page(item)
-                    if html2:
-                        source = detail_html(html2)
-                        raw_string = ','.join(source)  # list拆分成字符串并以逗号连接\
+                    html3 = download_page(item)
+                    if html3:
+                        source = parse_detail_pic(html3)
+                        raw_string = ','.join(source)
+                        print(raw_string)
                         r_content = str(raw_string) + '\n'
-                        fp_source.write(r_content)
-                        i = i + 1
-                        print(i)
+                        fp_pic.write(r_content)
+                        print(index+1)
 
-    fp.close()
-    fp_source.close()
+
+    #fp.close()
+    #fp_source.close()
 
 if __name__ == '__main__':
     main()
